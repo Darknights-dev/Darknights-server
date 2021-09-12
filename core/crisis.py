ff996eb12027f52504363068c7bc64e4443af52e
HGGG <31361289+WinHGGG@users.noreply.github.com>
2021-09-12 23:17:54 +0800

    small fix
    修复安卓ios源石不互通
    危机合约数据更新，增加危机合约商店

--- core/crisis.py
+++ core/crisis.py
@@ -13,8 +13,18 @@ def crisis_getInfo():
     """
     We need to sync crisis from hypergryph regularly.
     """
-    logger.info('Hit /crisis/getInfo')
+    logger.info('Hit /crisis/getInfo', request.environ.get('HTTP_X_FORWARDED_FOR'))
 
     medium = file.readFile('./serverData/crisis.json')
     medium['ts'] = int(time.time())
     return medium
+
+@route('/crisis/getGoodList', method='POST')
+def crisis_getGoodList():
+    """
+    """
+    logger.info('Hit /crisis/getGoodList', request.environ.get('HTTP_X_FORWARDED_FOR'))
+    
+    medium = file.readFile('./serverData/crisisGoodList.json')
+    return medium
+    return
--- core/sync.py
+++ core/sync.py
@@ -37,28 +37,23 @@ def account_syncData():
 
     Ts = int(time.time())
 
-    medium = file.readFile('./template/syncData.json')
-    stageTable = file.readFile('./serverData/stage_table.json')
-    
+    with open('./template/fullStage.json', 'r', encoding='utf-8') as f:
+        stages = f.read()
+    with open('./template/syncData.json', 'r', encoding='utf-8') as f:
+        empty_user = f.read()
 
-    emptyStage = {
-                    "stageId":"",
-                    "completeTimes":0,
-                    "startTimes":0,
-                    "practiceTimes":0,
-                    "state":3,
-                    "hasBattleReplay":0,
-                    "noCostCnt":0
-                }
-    fullStage = {}
-    for i in stageTable['stages'].keys():
-        fullStage[i] = emptyStage
-        fullStage[i]['stageId'] = i
+    medium = json.loads(empty_user)
+
     # we are using full stage
-    medium['user']['dungeon']['stages'] = fullStage
+    fullStage = json.loads(stages)
+    medium['user']['dungeon']['stages'] = fullStage['dungeon']['stages']
 
     # Load data from db
     medium['user']['status'] = user['status']
+    
+    # No difference between android and ios
+    medium['user']['status']['iosDiamond'] = medium['user']['status']['androidDiamond']
+    
     medium['user']['troop'] = user['troop']
     medium['user']['dexNav']['character'] = user['dexNav']['character']
     medium['user']['building'] = user['building']
